1857942a905c1ae8e08231fa88f633542412fde7
Kauan Gomes <Kauang13@gmail.com>
2023-08-08 18:04:32 -0300

    time em typescript

---
+++ src/componentes/Time/index.tsx
@@ -0,0 +1,37 @@
+import { IColaborador } from '../../shared/interfaces/IColaborador'
+import Colaborador from '../Colaborador'
+
+import './time.css'
+import React from 'react'
+
+interface TimeProps {
+    corPrimaria: string
+    corSecundaria: string
+    nome: string
+    colaboradores: IColaborador[]
+    time: string
+    
+}
+
+const Time = (props: TimeProps) => {
+
+    const css = { backgroundColor: props.corSecundaria}
+    return (
+
+        props.colaboradores.length > 0 && <section className='time' style={css}>
+            <h3 style={{ borderColor: props.corSecundaria }}>{props.nome}</h3>
+            <div className='colaboradores'>
+                {props.colaboradores.map((colaborador, indice) =>
+                    <Colaborador
+                        corDeFundo={props.corPrimaria}
+                        key={colaborador.nome}
+                        nome={colaborador.nome}
+                        cargo={colaborador.cargo}
+                        imagem={colaborador.imagem}
+                    />)}
+            </div>
+        </section>
+    )
+}
+
+export default Time
